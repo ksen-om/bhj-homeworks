f61f86c8428b8ff2e5c44d6ebbd06b4f7de87dec
ksen-om <ks.vl.om@gmail.com>
2022-09-13 09:09:59 +0300

    first commit

---
+++ dom/reveal/task.js
@@ -0,0 +1,17 @@
+let reveal = document.querySelectorAll('.reveal');
+
+for(let i = 0; i < reveal.length; i++) {
+    function scroll() {
+        let viewportHeight = window.innerHeight;
+        let revealTop = reveal[i].getBoundingClientRect().top;
+        let revealBottom = reveal[i].getBoundingClientRect().bottom;
+        console.log(revealTop, revealBottom);
+        console.log(viewportHeight);
+        if (reveal[i].classList.contains('reveal') === true && revealTop < viewportHeight) {
+            reveal[i].classList.add('reveal_active');
+        } else {
+            reveal[i].classList.remove('reveal_active');
+        }
+    }
+    document.addEventListener('scroll', scroll);
+}
