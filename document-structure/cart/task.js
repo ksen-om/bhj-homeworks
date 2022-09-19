1a203b155b5f19af3dc3136d62a2d34d60e72eca
ksen-om <102251688+ksen-om@users.noreply.github.com>
2022-09-19 14:10:57 +0300

    Update task.js

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,37 +1,46 @@
-const productAdd = document.querySelectorAll('.product__add');
-const productQuantityControls = document.querySelectorAll('.product__quantity-controls');
 const cartProducts = document.querySelector('.cart__products');
+const product = document.querySelectorAll('.product');
+let arr = [...product].map((item) => item.getAttribute('data-id'));
 
-for(let i = 0; i < productAdd.length; i++) {
-    productAdd[i].addEventListener('click', function() {
-        const product = this.closest('.product');
-        const id = product.dataset.id;
-        for (let i = 0; i < cartProducts.children.length; i++) {
-            if (cartProducts.children[i].dataset.id === id) {
-                let cartProductCount = cartProducts.children[i].querySelector('.cart__product-count');
-                cartProductCount.innerText = Number(cartProductCount.innerText) + Number(product.querySelector('.product__quantity-value').innerText);
-                return false;
-            }
+getInfoAboutProduct = (item) => {
+    const productControls = item.querySelector('.product__controls');
+    const productQuantityValue = productControls.querySelector('.product__quantity-value');
+    const productQuantityControl= item.querySelectorAll('.product__quantity-control');
+    
+productQuantityControl.forEach(item => {
+    item.addEventListener('click', (e) => {
+        if (e.target.classList.contains('product__quantity-control_dec')) {
+            if (productQuantityValue.textContent > 1) {
+                productQuantityValue.innerHTML = productQuantityValue.textContent - 1;
+        } 
+    } else {
+            productQuantityValue.innerHTML = +productQuantityValue.textContent + 1;
         }
-        cartProducts.insertAdjacentHTML('beforeend', `<div class='cart__product' data-id='${id}'>
-        <img class='cart__product-image' src='${product.querySelector('.product__image').src}'>
-        <div class='cart__product-count'>${product.querySelector('.product__quantity-value').innerText}</div>
-        </div>`);
-    })
-}
+    });
+});
 
-for (let i = 0; i < productQuantityControls.length; i++) {
-    productQuantityControls[i].addEventListener('click', function(event) {
-        let productQuantityValue = Number(this.querySelector('.product__quantity-value').innerText);
-        if (event.target.classList.contains('product__quantity-control_dec')) {
-            if (productQuantityValue > 1) {
-                productQuantityValue -= 1;
-            } else {
-                productQuantityValue = 1;
-            }
-        } else {
-            productQuantityValue += 1;
-        }
-        this.querySelector('.product__quantity-value').innerText = productQuantityValue;
-    })
-}
+const productAdd = productControls.querySelector('.product__add');
+productAdd.addEventListener ('click', () => {
+    const imgs = item.querySelector('.product__image').src;
+    const dataId = item.getAttribute('data-id');
+    const div = document.createElement('div');
+    div.setAttribute('id', dataId);
+    div.classList.add('cart__product');
+
+        div.innerHTML = `<img class='cart__product-image' src='${imgs}'>
+        <div class='cart__product-count'>${productQuantityValue.textContent}</div>`;
+
+const idRenderElem = arr.find(item => item === dataId);
+if (document.getElementById(idRenderElem)) {
+    const foundElem = document.getElementById(idRenderElem);
+    const prevElem = foundElem.querySelector('.cart__product-count').textContent;
+    foundElem.querySelector('.cart__product-count').innerHTML = +productQuantityValue.textContent + +prevElem;
+} else {
+    cartProducts.appendChild(div);
+    }
+});
+};
+
+product.forEach((el) => {
+    getInfoAboutProduct(el);
+});
